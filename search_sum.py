7c62e2d95ea698507907c6d85edbe8a3cac5995b
Maksim Pevnev <max.stak.job@gmail.com>
2022-05-22 22:24:05 +0400

    search_sum

--- search_sum.py
+++ search_sum.py
@@ -1,4 +1,5 @@
-#Надо бы проверить полным перебором
+# Надо бы проверить полным перебором
+# Реализовать Ckn
 from collections import defaultdict
 
 
@@ -18,19 +19,29 @@ def search_sum(values: list, n: int, n_sum: int):
                     v = v1 + v2 + v3
                     if v == n_sum:
                         return True
+    elif n == 4:
+        for i1, v1 in enumerate(values):
+            for i2, v2 in enumerate(values[i1 + 1:], start=i1 + 1):
+                for i3, v3 in enumerate(values[i2 + 1:], start=i2 + 1):
+                    for v4 in values[i3 + 1:]:
+                        v = v1 + v2 + v3 + v4
+                        if v == n_sum:
+                            return True
     else:
         n_bits = [int(v) for v in reversed(bin(n)[2:])]
         b_sum = defaultdict(list)
         for i, v in enumerate(values):
             b_sum[v].append({i})
 
-
         sum_sets = [b_sum] if n_bits[0] else []
 
         for b in n_bits[1:-1]:
-            b_sum = dict(merge_gen(b_sum, b_sum))
+            res = defaultdict(list)
+            for v, i in merge_gen(b_sum, b_sum):
+                res[v] += i
             if b:
-                sum_sets.append(b_sum)
+                sum_sets.append(res)
+            b_sum = res
 
         if n_bits.index(1) == len(n_bits) - 1:
             for v1, i1 in merge_gen(b_sum, b_sum):
@@ -40,7 +51,10 @@ def search_sum(values: list, n: int, n_sum: int):
             result_set = sum_sets[0]
 
             for sum_set in sum_sets[1:]:
-                result_set = dict(merge_gen(result_set, sum_set))
+                res = defaultdict(list)
+                for v, i in merge_gen(result_set, sum_set):
+                    res[v] += i
+                result_set = res
 
             for v1, i1 in merge_gen(b_sum, b_sum):
                 v2 = n_sum - v1
@@ -110,4 +124,9 @@ if __name__ == '__main__':
     values = [0, 1, 0, 1]
     n = 4
     n_sum = 2
+    assert search_sum(values, n, n_sum) is True
+
+    values = [-7, 2, -2, 3, -6, 2, 8, -3]
+    n = 4
+    n_sum = 11
     assert search_sum(values, n, n_sum) is True
