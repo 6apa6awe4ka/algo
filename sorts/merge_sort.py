e69c966276bd8e69a1f7ee968b746e2322df338c
Maksim Pevnev <max.stak.job@gmail.com>
2022-06-15 11:48:05 +0400

    merge_sort

--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -1,30 +1,55 @@
-def merge_sort(values):
+# need tests
+def merge_sort(values: list):
+    start = 0
+    end = len(values)
+    buf = values.copy()
+    split(values, buf, start, end)
+
+
+def split(values, buf, start, end):
+    if start - end <= 1:
+        return
+    bottom = (start + end) // 2
+    split(buf, values, start, bottom)
+    split(buf, values, bottom, end)
+    merge(buf, values, start, bottom, end)
+
+
+def merge(values, buf, start, bottom, end):
+    for i in range(start, end):
+        if bottom == end or values[start] > values[bottom]:
+            buf[i] = values[start]
+            start += 1
+        else:
+            buf[i] = values[bottom]
+            bottom += 1
+
+
+def merge_sort_non_recursive(values):
     n = len(values)
-    size = 2
+    buf = [None for i in range(0, n)]
+    size = 1
 
     while True:
-        buf = []
-        tail = n % step
+        start = 0
 
-        i1 = 0
-        i2 = size
+        while True:
+            bottom = start + size
+            end = bottom + size
+            if end >= n:
+                break
+            merge(values, buf, start, bottom, end)
+            start = end
 
-        while i2 < n:
-            i1_next = i2 + size
-            for ii in range(0, size * 2):
-                v1 = values[i1]
-                v2 = values[i2]
-                if i2 == i1_next or v1 > v2:
-                    i1 += 1
-                    buf.append(v1)
-                else:
-                    i2 += 1
-                    buf.append(v2)
-            i1 = i1_next
-            i2 = i1 + size
-
-        if tail > size:
-
+        if bottom >= n:
+            for i in range(start, n):
+                buf[i] = values[i]
+        else:
+            merge(values, buf, start, bottom, n)
 
         size *= 2
-        values = buf
+
+        if size >= n:
+            return buf
+
+        values, buf = buf, values
